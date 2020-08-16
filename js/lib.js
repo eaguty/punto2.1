51caa19079ceadc301eb43dc95e617c7587f7ffd
Adrian Gutierrez <adriangutierrezpabon@gmail.com>
2020-08-16 04:18:58 -0500

    Footer, menu, and carousels good

--- js/lib.js
+++ js/lib.js
@@ -1,50 +1,48 @@
 $(document).ready(function(){
-    /*$('#recipeCarousel').carousel();
-    
-    $('.estrategias .carousel-item').each(function(){
-            var minPerSlide = 4;
-            var next = $(this).next();
-            if (!next.length) {
-            next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-            
-            for (var i=0;i<minPerSlide;i++) {
-                next=next.next();
-                if (!next.length) {
-                    next = $(this).siblings(':first');
-                }
-                
-                next.children(':first-child').clone().appendTo($(this));
-            }
-    });
-*/
-    
     changeCssMenu ($(document).scrollTop());
-    
-    $(".nav-link").css("color", "#FFFFFF"); 
-    $(".navbar-brand").css("color", "#FFFFFF");
+    var windowsize = $(window).width();
+        if (windowsize > 991) {
+            $(".nav-link").css("color", "#FFFFFF"); 
+            $(".navbar-brand").css("color", "#FFFFFF");
+        }
+        else{
+            $(".nav-link").css("color", "#6A6A6A"); 
+            $(".navbar-brand").css("color", "#6A6A6A");
+        }
     $(window).scroll(function() {
-        changeCssMenu ($(document).scrollTop());
+         changeCssMenu ($(document).scrollTop());
     });
+    $( window ).resize(function() {
+        changeCssMenu ($(document).scrollTop());
+      });
 });
 
 var changeCssMenu = function(scroll) {
     if(parseInt(scroll) > 50.0) { 
         
         $("#menu1").css("background-color", "#ffffff"); 
-        $(".nav-link").css("color", "#000000");
+        $(".nav-link").css("color", "#6A6A6A");
         $(".cuenta").css("color", "#ffffff");
-        $(".navbar-brand").css("color", "#000000");
+        $(".navbar-brand").css("color", "#6A6A6A");
         $("#logo").attr("src","images/logo-nav-sticky.png");
 
 
     } else {
-        
-        $("#menu1").css("background-color", "transparent"); 
-        $(".nav-link").css("color", "#FFFFFF"); 
-        $(".navbar-brand").css("color", "#FFFFFF");
+        var windowsize = $(window).width();
+        if (windowsize > 991) {
+            $("#menu1").css("background-color", "transparent");
+            $(".nav-link").css("color", "#FFFFFF"); 
+            $(".navbar-brand").css("color", "#FFFFFF");
+        }
+        else{
+            $("#menu1").css("background-color", "transparent");
+            $(".nav-link").css("color", "#6A6A6A"); 
+            $(".navbar-brand").css("color", "#6A6A6A");
+            
+        }
         $("#logo").attr("src","images/logo-nav.png");
 
     }
 };
+
+
